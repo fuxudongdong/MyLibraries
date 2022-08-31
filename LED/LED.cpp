06f4847f12423f97b2ca895f99ad957e26082e92
傅旭栋 <704285881@qq.com>
2022-08-31 10:34:33 +0800

    Create LED.cpp

---
+++ LED/LED.cpp
@@ -0,0 +1,55 @@
+#include "LED.h"
+#include "Arduino.h"
+
+LED::LED(int p): pin(p) {
+  pinMode(pin, OUTPUT);
+  analogWriteRange(range);//占空比
+  analogWriteFreq(1000);//频率
+}
+
+LED::~LED() {
+
+}
+
+void LED::mode(int i) {
+  switch (i) {
+    case 0:
+      digitalWrite(pin, 1);
+      break;
+    case 1:
+      digitalWrite(pin, 0);
+      break;
+    case 2:
+      for (int i = 0; i < 1000; i++) {
+        analogWrite(pin, i);
+        delay(1);
+      }
+      delay(1000);
+      for (int i = 1000; i > 0; i--) {
+        analogWrite(pin, i);
+        delay(1);
+      }
+      break;
+    case 3:
+      digitalWrite(pin, !digitalRead(pin));
+      delay(500);
+      break;
+    case 4:
+      for (int i = 750; i < 1000; i++) {
+        analogWrite(pin, i);
+        delay(1);
+      }
+      for (int i = 1000; i > 750; i--) {
+        analogWrite(pin, i);
+        delay(1);
+      }
+      break;
+    case 5:
+      digitalWrite(pin, !digitalRead(pin));
+      break;
+  }
+}
+
+void LED::setRange(int myRange) {
+  range = myRange;
+}
