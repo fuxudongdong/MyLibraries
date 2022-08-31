482fcbf441b294c77aa070ba59b93e16f0150969
傅旭栋 <704285881@qq.com>
2022-08-31 10:34:31 +0800

    Create LED.h

---
+++ LED/LED.h
@@ -0,0 +1,16 @@
+#ifndef _LED__
+#define _LED__
+
+#include "Arduino.h"
+
+class LED {
+  private:
+    int pin = 2;
+    int range = 1000;
+  public:
+    LED(int pin);
+    ~LED();
+    void mode(int i);
+    void setRange(int range);
+};
+#endif
